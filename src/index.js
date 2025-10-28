69244d4a17f8dd2efc3787a948c7ebe07b0c3db4
Diogo-Milani <diogootaviomilani@outlook.com>
2025-10-28 09:11:02 -0300

    Atualizacao

--- src/index.js
+++ src/index.js
@@ -60,33 +60,3 @@ const PORT = process.env.PORT || 3333
 app.listen(PORT, () => {
     console.log (`Servidor rodando na porta ${PORT}`)
 })
-
-
-
-// const listaDeClientes = [
-//     {id: 1, nome: 'Diogo Milani', email: 'diogootaviomilani@outlook.com'},
-//     {id: 2, nome: 'Maria Santos', email: 'maria.santos@example.com'},
-//     {id: 3, nome: 'Luiz Carlos', email: 'luiz.carlos@example.omc.br'}
-// ]
-
-// app.get('/clientes', (req, res) => {
-//     res.json(listaDeClientes)
-// })
-
-// app.get('/clientes/:id', (req, res) => {
-//     const idDoCliente = parseInt(req.params.id)
-//     const cliente = listaDeClientes.find (c => c.id === idDoCliente)
-//     if (cliente) {
-//         res.json(cliente)
-//     } else {
-//         res.status(404).json ({mensagem: 'Cliente não encontrado'})
-//     }
-// })
-
-// app.post('/clientes', (req,res) => {
-//     const novoCliente = req.body
-
-//     console.log('Recebemos um novo cliente', novoCliente)
-
-//     res.json({message: `Cliente ${novoCliente.nome} cadastrado com sucesso!`, data: novoCliente})
-// })
